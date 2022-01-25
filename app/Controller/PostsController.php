27eec497f368ca4c3fe89e9fc727a4978b739744
luqmansolihin <luqmansolihin.jte@gmail.com>
2022-01-25 19:12:06 +0700

    First Commit

---
+++ app/Controller/PostsController.php
@@ -0,0 +1,118 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Posts Controller
+ *
+ * @property Post $Post
+ * @property PaginatorComponent $Paginator
+ */
+class PostsController extends AppController {
+
+/**
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator');
+
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index() {
+		$this->Post->recursive = 0;
+		$this->set('posts', $this->Paginator->paginate());
+	}
+
+/**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		if (!$this->Post->exists($id)) {
+			throw new NotFoundException(__('Invalid post'));
+		}
+		$options = array('conditions' => array('Post.' . $this->Post->primaryKey => $id));
+		$this->set('post', $this->Post->find('first', $options));
+	}
+
+	public function isAuthorized($user) {
+		// All registered users can add posts
+		if ($this->action === 'add') {
+			return true;
+		}
+	
+		// The owner of a post can edit and delete it
+		if (in_array($this->action, array('edit', 'delete'))) {
+			$postId = (int) $this->request->params['pass'][0];
+			if ($this->Post->isOwnedBy($postId, $user['id'])) {
+				return true;
+			}
+		}
+	
+		return parent::isAuthorized($user);
+	}
+
+/**
+ * add method
+ *
+ * @return void
+ */
+public function add() {
+    if ($this->request->is('post')) {
+        $this->request->data['Post']['user_id'] = $this->Auth->user('id');
+        if ($this->Post->save($this->request->data)) {
+            $this->Flash->success(__('Your post has been saved.'));
+            return $this->redirect(array('action' => 'index'));
+        }
+    }
+}
+
+/**
+ * edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function edit($id = null) {
+		if (!$this->Post->exists($id)) {
+			throw new NotFoundException(__('Invalid post'));
+		}
+		if ($this->request->is(array('post', 'put'))) {
+			if ($this->Post->save($this->request->data)) {
+				$this->Flash->success(__("The post has been saved."));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Flash->error(__("The post could not be saved. Please, try again."));
+			}
+		} else {
+			$options = array('conditions' => array('Post.' . $this->Post->primaryKey => $id));
+			$this->request->data = $this->Post->find('first', $options);
+		}
+	}
+
+/**
+ * delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function delete($id = null) {
+		if (!$this->Post->exists($id)) {
+			throw new NotFoundException(__('Invalid post'));
+		}
+		$this->request->allowMethod('post', 'delete');
+		if ($this->Post->delete($id)) {
+			$this->Flash->success(__("The post has been deleted."));
+		} else {
+			$this->Flash->error(__("The post could not be deleted. Please, try again."));
+		}
+		return $this->redirect(array('action' => 'index'));
+	}
+}
